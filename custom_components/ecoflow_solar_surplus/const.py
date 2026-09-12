4a3f6afda25e9843a33437c265ba3fa99183c662
chrisj500 <69051459+chrisj500@users.noreply.github.com>
2026-09-11 23:11:48 -0400

    Bump integration version to 0.1.1

--- custom_components/ecoflow_solar_surplus/const.py
+++ custom_components/ecoflow_solar_surplus/const.py
@@ -4,7 +4,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "ecoflow_solar_surplus"
 NAME = "EcoFlow Solar Surplus Controller"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 PLATFORMS = [Platform.SENSOR, Platform.BINARY_SENSOR]
 
 CONF_SITE_GRID_POWER = "site_grid_power"
